cd2af0ab724931f8cb60ca89200a5719cf4dd382
Manu Vleurick <manu.vleurick@student.hogent.be>
2023-01-16 22:53:17 +0100

    test

--- H7_Oef/backup.sh
+++ H7_Oef/backup.sh
@@ -6,9 +6,23 @@ set -o errexit
 dest_dir="/tmp"
 source_dir="$HOME"
 
-# Get the current timestamp
-timestamp=$(date +%Y%m%d%H%M)
-
 function usage {
-  
+  echo "Usage: backup.sh [OPTIES] [DIR]"
+  echo "-h|-?|--help = drukt uitleg over commando af"
+  echo "-d|--destination = bestemming van backup"
+  echo "DIR = bronmap van backup"
 }
+
+function create_backup {
+  # Current timestamp
+  timestamp=$(date +%Y%m%d%H%M)
+  echo "$timestamp"
+  #tar -czf "$dest_dir"/backup_"$timestamp".tar.gz "$source_dir"
+}
+
+function main {
+    create_backup "$@"
+    echo "$?"
+}
+
+main "$@"
