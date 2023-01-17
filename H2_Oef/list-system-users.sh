29823c4134a457951e076b7650abea41962f2494
Manu Vleurick <manu.vleurick@student.hogent.be>
2023-01-17 09:57:52 -0500

    H2 scripts shellcheck ready

--- H2_Oef/list-system-users.sh
+++ H2_Oef/list-system-users.sh
@@ -1,11 +1,11 @@
-#!bin/bash
+#!/bin/bash
 LEN_FILE=$(wc -l /etc/passwd | cut -d " " -f 1)
-for ((i=1;i<=$LEN_FILE;i+=1));
+for ((i=1;i<=LEN_FILE;i+=1));
 do
 GEBR=$(head -n $i /etc/passwd | tail -1 | cut -d ":" -f 1)
 UID_G=$(head -n $i /etc/passwd | tail -1 | cut -d ":" -f 3)
-if [[ $UID_G < 1000 ]]; then
-echo $GEBR
-echo $UID_G
+if [[ $UID_G -lt 1000 ]]; then
+echo "$GEBR"
+echo "$UID_G"
 fi
 done
